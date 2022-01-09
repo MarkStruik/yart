6115e441d5608ab2b7bbc516bd227d7f8dad5701
Mark Struik <mark.struik@devops.ncia.nato.int>
2022-01-09 17:01:54 +0000

    added a fireball to the game

--- components/ai.py
+++ components/ai.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
-
-from typing import List, Tuple, TYPE_CHECKING
+import random
+from typing import List, Optional, Tuple, TYPE_CHECKING
 
 import numpy as np
 import tcod
 from tcod.path import Pathfinder
 
 
-from actions import Action, MeleeAction, MovementAction, WaitAction
+from actions import Action, MeleeAction, MovementAction, WaitAction, BumpAction
 
 if TYPE_CHECKING:
     from Entity import Actor
@@ -42,6 +42,43 @@ class BaseAI(Action):
         path: List[List[int]] = pathfinder.path_to((dest_x, dest_y))[1:].tolist()
 
         return [(index[0], index[1]) for index in path]
+
+class ConfusedEnemy(BaseAI):
+    """
+    A confused enemy will stumble around aimlessly for a
+    given number of tuns, then revert back to its previous AI.
+    
+    if an actor accupies a tile it is randomly moving into it will attach
+    """
+    def __init__(self, entity: Actor, previous_ai: Optional[BaseAI], turns_remaining: int) -> None:
+        super().__init__(entity)
+        self.previous_ai = previous_ai
+        self.turns_remaining = turns_remaining
+
+    def perform(self) -> None:
+        # revert the AI back to the original state if the effect has run its course
+        if self.turns_remaining <= 0:
+            self.engine.message_log.add_message(
+                f"The {self.entity.name} is no longer confused."
+            )
+            self.entity.ai = self.previous_ai
+        else:
+            # Pick a random direction
+            direction_x, direction_y = random.choice(
+                [
+                    (-1,-1),
+                    (0,-1),
+                    (1,-1),
+                    (-1,0),
+                    (1,0),
+                    (-1,1),
+                    (0,1),
+                    (1,1),
+                ]
+            )
+            self.turns_remaining -= 1
+
+            return BumpAction(self.entity, direction_x, direction_y,).perform()
 
 class HostileEnemy(BaseAI):
     def __init__(self, entity: Actor) -> None:
